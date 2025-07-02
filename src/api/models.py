ff69d285c3feef2047215991ec9102a04759a73c
Evens Antoine <evens7antoine@gmail.com>
2025-07-02 23:52:53 +0000

    done with models and routes

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,54 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Float, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    name = Mapped[str](String(100), nullable=False)
+    picture: Mapped[str] = mapped_column(nullable=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    expenses=relationship("Expense", back_populates="user")
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "picture": self.picture,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "is_active": self.is_active
+        }
+    
+
+class Expense(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    transaction_date: Mapped[datetime] = mapped_column(nullable=False)
+    transaction_category: Mapped[str] = mapped_column(String(120), unique=False, nullable=False)
+    transaction_description: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    transaction_currency: Mapped[str] = mapped_column(String(120), unique=False, nullable=False)
+    transaction_amount: Mapped[Float] = mapped_column(nullable=False)
+    is_recurring: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+
+    expenses=relationship("User", back_populates="expense")
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    user=relationship("User", back_populates="expense")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "transaction_date": self.transaction_date,
+            "transaction_category": self.transaction_category,
+            "transaction_description": self.transaction_description,
+            "transaction_currency": self.transaction_currency,
+            "transaction_amount": self.transaction_price,
+            "is_recurring": self.is_recurring,
+            "is_active": self.is_active
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,12 +4,11 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_cors import cross_origin
 
 api = Blueprint('api', __name__)
-
-# Allow CORS requests to this API
-CORS(api)
 
 
 @api.route('/hello', methods=['POST', 'GET'])
@@ -20,3 +19,150 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+# Allow User to log in with email and password from User account
+@api.route('/token', methods=['POST'])
+def token():
+    try:
+        data = request.get_json()
+        email = data.get('email')
+        password = data.get('password')
+
+        if not email or not password:
+            return jsonify({"msg": "Missing email or password"}), 400
+
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            access_token = create_access_token(identity=f"user:{user.id}")
+            return jsonify(
+                access_token=access_token,
+                user={
+                    "id": user.id,
+                    "email": user.email,
+                    "name": user.name
+                },
+                role="user"
+            ), 200
+        
+        return jsonify({"msg": "Bad credentials"}), 401
+
+    except Exception as e:
+        print("Error in /api/token:", e)
+        return jsonify({"error": "Internal Server Error"}), 500
+    
+
+# Allows logged in user to see their information
+@api.route('/profile', methods=['GET'])
+@jwt_required()
+def get_profile():
+    identity = get_jwt_identity()
+    print("JWT identity:", identity)
+
+    try:
+        role, id_str = identity.split(":")
+        user_id = int(id_str)
+    except Exception:
+        return jsonify({"msg": "Invalid token format"}), 400
+
+    if role == "user":
+        user = User.query.get(user_id)
+        if user:
+            return jsonify({**user.serialize(), "role": "user"}), 200
+        
+
+# Allows users to edit their email and name/title
+@api.route('/profile', methods=['PUT'])
+@jwt_required()
+def update_profile():
+    identity = get_jwt_identity()
+    print("JWT identity:", identity)
+
+    try:
+        role, id_str = identity.split(":")
+        user_id = int(id_str)
+    except Exception:
+        return jsonify({"msg": "Invalid token format"}), 400
+
+    data = request.get_json()
+
+    new_email = data.get("email")
+
+    if role == "user":
+        new_name = data.get("name")
+        if not new_name or not new_email:
+            return jsonify({"msg": "Name and email are required"}), 400
+
+        user = User.query.get(user_id)
+        if user:
+            existing_user = User.query.filter(User.email == new_email, User.id != user.id).first()
+            if existing_user:
+                return jsonify({"msg": "Email is already in use"}), 400
+            
+            user.name = new_name
+            user.email = new_email
+            db.session.commit()
+            return jsonify(user.serialize()), 200
+        
+
+# Allows users to edit their current password and update their new one
+@api.route('/profile/password', methods=['PUT'])
+@jwt_required()
+def update_password():
+    identity = get_jwt_identity()
+    role, id_str = identity.split(":")
+    user_id = int(id_str)
+
+    data = request.get_json()
+    current_password = data.get("current_password")
+    new_password = data.get("new_password")
+
+    if not current_password or not new_password:
+        return jsonify({"msg": "Current and new password required"}), 400
+
+    if role == "user":
+        user = User.query.get(user_id)
+        if not user or not check_password_hash(user.password, current_password):
+            return jsonify({"msg": "Current password is incorrect"}), 400
+        user.password = generate_password_hash(new_password)
+        db.session.commit()
+        return jsonify({"msg": "Password updated successfully"}), 200
+    
+
+# Allows users to signup their account
+@api.route('/signup', methods=['POST'])
+@cross_origin()
+def signup():
+    data = request.get_json()
+    name = data.get("name")
+    email = data.get("email")
+    password = data.get("password")
+
+    if not name or not email or not password:
+        return jsonify({"message": "Name, email and password required"}), 400
+
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        return jsonify({"message": "User already exists"}), 409
+
+    hashed_password = generate_password_hash(password)
+    new_user = User(
+        name=name,
+        email=email,
+        password=hashed_password,
+        is_active=True,
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User registered successfully, You may Login."}), 201    
+        
+
+# Delete a User
+@api.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify("user has been deleted"), 200
